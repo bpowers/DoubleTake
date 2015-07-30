352e6e523cf78bbd7c1c46a224208926d08b7632
Bobby Powers <bobbypowers@gmail.com>
2015-07-29 22:38:47 -0400

    tests/simple_overflow: reformat

--- tests/simple_overflow/main.c
+++ tests/simple_overflow/main.c
@@ -4,14 +4,17 @@
 extern void overflow (void * buf, int actualSize, int overflowSize);
 
 int main(int argc, char** argv) {
-	char buff[256];
-	fprintf(stderr, "In the beginning of main function\n");
+  char buff[256];
+
+  fprintf(stderr, "In the beginning of main function\n");
+
   int* p = (int*)malloc(sizeof(char));
-	fprintf(stderr, "after malloc, p is %p\n", p);
+
+  fprintf(stderr, "after malloc, p is %p\n", p);
+
   overflow (p, sizeof(char), 4);
-  //  *p = 12345;
-//  free(p);
-	fprintf(stderr, "Right before the return of main function\n");
+
+  fprintf(stderr, "Right before the return of main function\n");
 	
   return 0;
 }
